a3777582416f8188163bfaaff924d7f08dc592a5
Téo Salmeron <teo.salmeron2601@gmail.com>
2024-04-16 18:00:15 +0200

    Trying to fix cookies 2

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.use(cors({
     credentials: true
 }))
 
+app.get("/", async (req, res) => {
+    const token = req.cookies.token
+    return res.json({token: token})
+})
+
 // ROUTES
 const loginRouter = require("./routes/Login")
 app.use("/login", loginRouter)
